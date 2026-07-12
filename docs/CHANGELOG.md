34911c07bf3a328a7d9098e457903f71d02e2748
Chris Huber <chubes@extrachill.com>
2026-07-12 03:09:39 +0000

    release: v0.8.0

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -5,6 +5,15 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.8.0] - 2026-07-12
+
+### Added
+- Recently Shipped homepage strip
+- restructure homepage as live network dashboard
+
+### Fixed
+- make responsive card grids overflow-safe (min(100%, floor))
+
 ## [0.7.2] - 2026-06-27
 
 ### Changed
--- extrachill-blog.php
+++ extrachill-blog.php
@@ -3,7 +3,7 @@
  * Plugin Name: Extra Chill Blog
  * Plugin URI: https://extrachill.com
  * Description: Blog-specific functionality for extrachill.com (Blog ID 1). Provides secondary header navigation, homepage customizations, and blog-specific templates.
- * Version: 0.7.2
+ * Version: 0.8.0
  * Author: Chris Huber
  * Author URI: https://chubes.net
  * License: GPL v2 or later
@@ -22,7 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'EXTRACHILL_BLOG_VERSION', '0.7.2' );
+define( 'EXTRACHILL_BLOG_VERSION', '0.8.0' );
 define( 'EXTRACHILL_BLOG_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'EXTRACHILL_BLOG_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
